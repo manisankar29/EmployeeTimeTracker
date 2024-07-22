cb7d46ca7f367aff5408a6af5f1aa7efc5c8fa48
mani sankar pasala <manisankar249@gmail.com>
2024-07-22 12:24:12 +0530

    updated

--- src/com/timetracker/dao/UserDAO.java
+++ src/com/timetracker/dao/UserDAO.java
@@ -21,6 +21,11 @@ public class UserDAO {
                 user.setUserId(resultSet.getInt("user_id"));
                 user.setUsername(resultSet.getString("username"));
                 user.setPassword(resultSet.getString("password"));
+                user.setName(resultSet.getString("name"));
+                user.setEmployeeId(resultSet.getString("employeeId"));
+                user.setIdProof(resultSet.getString("idProof"));
+                user.setPhoneNumber(resultSet.getString("phoneNumber"));
+                user.setEmail(resultSet.getString("email"));
                 user.setRole(resultSet.getString("role"));
             }
         } catch (SQLException e) {
@@ -31,12 +36,20 @@ public class UserDAO {
 
     public void addUser(User user) {
         try (Connection connection = DatabaseConnection.getConnection()) {
-            String sql = "INSERT INTO Users (username, password, role) VALUES (?, ?, ?)";
+            String sql = "INSERT INTO Users (username, password, name, employeeId, idProof, phoneNumber, email, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setString(1, user.getUsername());
             statement.setString(2, user.getPassword());
-            statement.setString(3, user.getRole());
-            statement.executeUpdate();
+            statement.setString(3, user.getName());
+            statement.setString(4, user.getEmployeeId());
+            statement.setString(5, user.getIdProof());
+            statement.setString(6, user.getPhoneNumber());
+            statement.setString(7, user.getEmail());
+            statement.setString(8, user.getRole());
+            int rows = statement.executeUpdate();
+            if (rows > 0) {
+                System.out.println("A new user has been inserted.");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,6 +1,7 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
@@ -8,15 +9,16 @@
 </head>
 <body>
     <h2>Login</h2>
-    <form action="user?action=login" method="post">
+    <form action="user" method="post">
+        <input type="hidden" name="action" value="login">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required><br>
-
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required><br>
-
-        <input type="submit" value="Login">
+        <button type="submit">Login</button>
     </form>
-    <a href="register.jsp">Register</a>
+    <c:if test="${param.error != null}">
+        <p style="color:red;">${param.error}</p>
+    </c:if>
 </body>
 </html>
